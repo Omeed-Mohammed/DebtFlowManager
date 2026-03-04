becac8ead0b42da74bf0763d3df35dbd04c01777
omeed mohammed <omeed1987@gmail.com>
2026-03-04 02:32:35 +0100

    Implement Invoice retrieval by ReceiptNo (SP + DTO + DAL + BL)

---
+++ DebtFlowManager_BL/Suppliers/SupplierInvoiceService.cs
@@ -0,0 +1,21 @@
+﻿using DebtFlowManager_DA.Suppliers;
+using DTO_Layer.Suppliers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebtFlowManager_BL.Suppliers
+{
+    public class SupplierInvoiceService
+    {
+        public static SupplierPaymentInvoiceDTO GetInvoiceByReceiptNo(string receiptNo)
+        {
+            if (string.IsNullOrEmpty(receiptNo))
+                throw new ArgumentNullException(nameof(receiptNo));
+
+            return SupplierInvoicesRepository.GetInvoiceByReceiptNo(receiptNo);
+        }
+    }
+}
